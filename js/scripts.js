47298ce0a7e1fbea3f34595e0ba7f57916f0e4ad
Gary Bermudez <garybm.17@gmail.com>
2019-01-18 12:36:05 -0800

    add var for each selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-  $('#survey form').submit(function(event) {
-    var locationSelect = $("select#location").val();
+  $("form#survey").submit(function(event) {
+    var location = parseInt$('select#location').val();
+    var company = parseInt$("select#company").val();
+    var teams = parseInt$("select#teams").val();
+    var projects = parseInt$("select#projects").val();
+    var words = parseInt$("select#words").val();
+    var result = location + company + teams + projects + words;
 
+    if (result > 5) {
+    $('#result1').show();
+    $('#result2').hide();
+    $('#result3').hide();
 
-event.preventDefault();
-  }
-})
+    event.preventDefault();
+  });
+});
