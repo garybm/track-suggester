8dfd27a3fe819834bab1b159598912bb8aa3bdb4
Gary Bermudez <garybm.17@gmail.com>
2019-01-18 12:54:20 -0800

    add the if statment and it's now giving a result

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
-    var location = parseInt$('select#location').val();
-    var company = parseInt$("select#company").val();
-    var teams = parseInt$("select#teams").val();
-    var projects = parseInt$("select#projects").val();
-    var words = parseInt$("select#words").val();
+    var location = parseInt($("#location").val());
+    var company = parseInt($("#company").val());
+    var teams = parseInt($("#teams").val());
+    var projects = parseInt($("#projects").val());
+    var words = parseInt($("#words").val());
     var result = location + company + teams + projects + words;
 
     if (result > 5) {
     $('#result1').show();
     $('#result2').hide();
     $('#result3').hide();
+  } else if (result > 4 && result < 8) {
+    $('#result1').show();
+    $('#result2').show();
+    $('#result3').show();
+  } else {
+    $('#result1').hide();
+    $('#result2').show();
+    $('#result3').show();
+  }
 
     event.preventDefault();
+
+    $("#location").val("");
+    $("#company").val("");
+    $("#teams").val("");
+    $("#projects").val("");
+    $("#words").val("");
   });
 });
